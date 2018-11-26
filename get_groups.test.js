685a9ca20839f350edd765958b61e5201bedce63
martina-battisti <martina.battisti-1@studenti.unitn.it>
2018-11-26 12:38:06 +0100

    aggiunta test post /user

--- get_groups.test.js
+++ get_groups.test.js
@@ -1,5 +1,9 @@
 const f1groups = require('./get_groups').f1groups
 
-test('group 0 corrisponde', () => {
+test('get group corrisponde', () => {
 	expect(f1groups('a')).toBe('a');
+});
+
+test('post group corrisponde', () => {
+	expect(post('prova')).toEqual({ id: 'prova', name: 'prova' });
 });
