cba02b74eab2ee5a0243d31a54fe2d309a87e4c3
Andrea Carrara <carrara.and@gmail.com>
2018-12-02 15:07:33 +0100

    cambiato exams

--- exams.js
+++ exams.js
@@ -10,8 +10,21 @@ var get_exams = (x) => {
 }
 
 var post_exams = (body,i) => {  
-    const new_exam = {examid:i, creator:body.creator, tasks:body.tasks, groups:body.groups};
-    return(new_exam);
+    if(body==null || isNaN(i)) {
+		return('errore')
+	} else if(body.creator == false || body.creator == null){
+        return('errore')
+    }
+    else if(body.tasks == false || body.tasks == null){
+        return('errore')
+    } else if(body.groups == false || body.groups == null){
+        return('errore')
+    } else{
+        const new_exam = {examid:i, creator:body.creator, tasks:body.tasks, groups:body.groups};
+        return(new_exam);
+    }
+
+    
 }
 	
 module.exports={get_exams,post_exams}
